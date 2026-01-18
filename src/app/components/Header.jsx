32652178366310787ad128f0c4391215ac96f330
Mayank <mayankrawat1424@gmail.com>
2026-01-18 20:19:09 +0530

    Responsive Styling for display

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between px-32 py-4 border-b-2 bg-white text-black items-center mb-32">
+    <div className="flex justify-between py-4 bg-white text-black items-center mb-32 px-8 lg:px-32">
       <h1 className="font-semibold font-serif text-xl">Lock_In .</h1>
       <ul className="text-sm">
         <a href="https://www.mayankrawat.info/" className="hover:underline ">
